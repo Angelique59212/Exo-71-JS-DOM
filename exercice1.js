f58c9831281b45816926a267025068f57cac3edc
Angelique59212 <92031713+Angelique59212@users.noreply.github.com>
2021-11-17 21:58:18 +0100

    dom

--- exercice1.js
+++ exercice1.js
@@ -1,11 +1,16 @@
-let monDiv = .createElement('div');
+let monDiv = document.createElement('div');
+monDiv.innerHTML = "Ce div a été ajouté en utilisant javascript";
 
 document.getElementById('viewport').appendChild(monDiv);
 
+let secondDiv = document.createElement("div");
+document.getElementById("viewport").prepend(secondDiv);
 
+secondDiv.style.background = "red";
+secondDiv.style.width = "100px";
+secondDiv.style.height = "100px";
 
-
-
-
+let span = document.getElementById("aSupprimer");
+span.remove();
 
 
